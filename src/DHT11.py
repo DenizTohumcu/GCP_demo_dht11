aef8c9580767475b64b140db118ae5cbf8a71dfc
DenizTohumcu <deniz.tohumcu93@gmail.com>
2021-09-14 00:16:15 +0200

    _edge_ functions

--- src/DHT11.py
+++ src/DHT11.py
@@ -73,3 +73,51 @@ class DHT11(object):
         diff =pigpio.tickDiff(self.high_tick, tick)
         handler(tick, diff)
 
+    def _edge_RISE(self, tick ,diff):
+        """
+        Handle Rise signal.
+        """
+        val = 0
+        if diff >= 50:
+            val = 1
+        if diff >= 200: # Bad bit?
+            self.checksum = 256 # Force bad checksum
+
+        if self.bit >= 40: # Message complete
+            self.bit = 40
+        elif self.bit >=32: # In checksum byte
+            self.checksum = (self.checksum << 1) + val
+            if self.bit == 39:
+                #40th bit received
+                self.pi.set_watchdog(self.gpio, 0)
+                total = self.humidity + self.temperature
+                # is checksum ok ?
+                if not (total & 255) == self.checksum:
+                    raise
+        elif 16 <= self.bit < 24: # in temperature byte
+            self.temperature = (self.temperature << 1) + val
+        elif 0 <= self.bit < 8: # in humidity byte
+            self.humidity = (self.humidity << 1) + val
+        else: # skip header bits 
+            pass
+        self.bit += 1
+
+    def _edge_FALL(self, tick, diff):
+        """
+        Handle Fall signal
+        """
+        self.high_tick = tick
+        if diff <= 250000:
+            return
+        self.bit = -2
+        self.checksum = 0
+        self.temperature = 0
+        self.humidity = 0
+
+    def _edge_EITHER(self, tick, diff):
+        """
+        Handle Either signal.
+        """
+        self.pi.set_watchdog(self.gpio, 0)
+
+        
