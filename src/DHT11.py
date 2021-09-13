3e4e5657d84bca29259bac09efff02d1c37b91ed
DenizTohumcu <deniz.tohumcu93@gmail.com>
2021-09-13 23:53:44 +0200

    __init__, setup and callback functions

--- src/DHT11.py
+++ src/DHT11.py
@@ -1,3 +1,75 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#Importing libraries
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import time
+import pigpio
+
+class DHT11(object):
+    """
+    The DHT11 class is a stripped version of the DHT22 sensor code by joan2937.
+    You can find the initial implementation here:
+    - https://github.com/srounet/pigpio/tree/master/EXAMPLES/Python/DHT22_AM2302_SENSOR
+    In this demo it is also a modified version of that pigpio repo by joan2937.
+
+    example code:
+    >>> pi = pigpio.pi()
+    >>> sensor = DHT11(pi, 4) # 4 is the data GPIO pin connected to your sensor
+    >>> for response in sensor:
+    ....    print("Temperature: {}".format(response['temperature']))
+    ....    print("Humidity: {}".format(response['humidity']))
+    """
+
+    def __init__(self, pi, gpio):
+        """
+        pi (pigpio): an instance of pigpio
+        gpio (int): gpio pin number
+        """
+        #Initializing the values of the instance
+        self.pi = pi
+        self.gpio = gpio
+        self.high_tick = 0
+        self.bit = 40
+        self.temperature = 0
+        self.humidity = 0
+        self.either_edge_cb = None
+        self.setup()
+
+    def setup(self):
+        """
+        Clears the internal gpio pull-up/down resistor.
+        Kills any watchdogs.
+        """
+
+        self.pi.set_pull_up_down(self.gpio, pigpio.PUD_OFF)
+        self.pi.set_watchdog(self.gpio, 0)
+        self.register_callbacks()
+
+    def register_callbacks(self):
+        """
+        Monitors RISING_EDGE changes using callback.
+        """
+        self.either_edge_cb = self.pi.callback(
+            self.gpio,
+            pigpio.EITHER_EDGE,
+            self.either_edge_callback
+        )
+    
+    def either_edge_callback(self, gpio, level, tick):
+        """
+        Either Edge callbacks, called each time the gpio edge changes.
+        Accumulate the 40 data bits from the dht11 sensor.
+        """
+
+        level_handlers = {
+            pigpio.FALLING_EDGE: self._edge_FALL,
+            pigpio.RISING_EDGE: self._edge_RISE,
+            pigpio.EITHER_EDGE: self._edge_EITHER
+        }
+        handler = level_handlers[level]
+        diff =pigpio.tickDiff(self.high_tick, tick)
+        handler(tick, diff)
+
